1d3b5b6e3b132100cdc6303f7946ce70b1c15f62
Eric Pezzulo <eric.pezzulo@gmail.com>
2024-08-10 08:15:49 -0400

    playing with animations

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import {
   trigger,
   state,
@@ -6,20 +6,20 @@ import {
   animate,
   transition,
 } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-test-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './test-component.component.html',
   animations: [
     trigger('openClose', [
       state(
         'open',
         style({
-          // height: '200px',
           opacity: 1,
-  
         })
       ),
       state(
@@ -28,20 +28,80 @@ import {
           // height: '100px',
           opacity: 0,
           transform: 'translatey(35px)',
-          
-      
         })
       ),
-      transition('open => closed', [animate('1s')]),
-      transition('closed => open', [animate('1s')]),
+      transition('open => closed', [animate('.15s')]),
+      transition('closed => open', [animate('.15s')]),
+    ]),
+    trigger('openDialog', [
+      state(
+        'open',
+        style({
+          opacity: 1,
+          // display: 'fixed',
+          transform: 'translateY(0px)',
+          zIndex: 20,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          opacity: 0,
+          zIndex: -10,
+          transform: 'translateY(35px)',
+        })
+      ),
+      transition('open => closed', animate('.2s ease-out')),
+      transition('closed => open', animate('.2s ease-in')),
+    ]),
+    trigger('openDropShadow', [
+      state(
+        'open',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          opacity: 0,
+          zIndex: -10,
+        })
+      ),
+      transition('open => closed', animate('.1s')),
+      transition('closed => open', animate('1s')),
     ]),
   ],
 })
 export class TestComponentComponent {
-
-  isOpen:boolean = true;
-
-  toggle(){
-    this.isOpen = !this.isOpen
+  isOpen: boolean = false;
+  isDialogOpen: boolean = false;
+  isDropShadowOpen: boolean = false;
+  toastMsg: string  = '';
+  toggle(message: string) {
+    this.toastMsg = message;
+    this.isOpen = !this.isOpen;
+    setTimeout(() => {
+      this.isOpen = false;
+    }, 3000);
+  }
+  toggleDialog() {
+    this.isDropShadowOpen = !this.isDropShadowOpen;
+    this.isDialogOpen = !this.isDialogOpen;
+  }
+  handleCloseDialog() {
+    this.isDropShadowOpen = false;
+    this.isDialogOpen = false;
+  }
+  handleSave() {
+    this.handleCloseDialog()
+    setTimeout(()=>{
+      this.toggle(this.toastMsg);
+    },500)
+    
+  }
+  handleInput(e: Event) {
+    const inputElement = e.target as HTMLInputElement;
+    this.toastMsg = inputElement.value
   }
 }
