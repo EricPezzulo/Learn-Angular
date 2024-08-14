ca3ea1211401b1de5e75c0d9bf4c9cf9b4867b3b
Eric Pezzulo <eric.pezzulo@gmail.com>
2024-08-14 19:45:50 -0400

    adding some UI fixes

--- src/app/test-component/test-component.ts
+++ src/app/test-component/test-component.ts
@@ -1,36 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { ReusableDialogComponent } from '../reusable-dialog/reusable-dialog.component';
-import { DialogComponent } from '../dialog/dialog.component';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
 @Component({
   selector: 'app-test-component',
   standalone: true,
-  imports: [CommonModule, MatIconModule, FormsModule, ReusableDialogComponent],
-  templateUrl: './test-component.html',
-  animations: [
-    trigger('addItem', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(100px)' }),
-        animate(
-          '200ms 200ms',
-          style({ opacity: 1, transform: 'translateY(0px)' })
-        ),
-      ]),
-      transition(':leave', [
-        style({ opacity: 1, transform: 'translateY(0px)' }),
-        animate('200ms', style({ opacity: 0, transform: 'translateY(100px)' })),
-      ]),
-    ]),
+  imports: [
+    CommonModule,
+    MatIconModule,
+    FormsModule,
+    ReusableDialogComponent,
+    NgClass,
+    TodoItemComponent,
   ],
+  templateUrl: './test-component.html',
 })
 export class TestComponent {
   isOpen: boolean = false;
@@ -40,7 +25,7 @@ export class TestComponent {
   inputText: string = '';
 
   @ViewChild(ReusableDialogComponent) dialog!: ReusableDialogComponent;
-
+  elemList: elemListType[] = [];
   toggle(message: string) {
     this.toastMsg = message;
     this.isOpen = !this.isOpen;
@@ -49,27 +34,9 @@ export class TestComponent {
     }, 3000);
   }
   toggleDialog() {
-    // this.isDropShadowOpen = true;
-    // this.isDialogOpen = true;
     this.dialog.openDialog();
   }
 
-  elemList: elemListType[] = [];
-
-  deleteElem(id: string) {
-    this.elemList = this.elemList.filter((el) => el.id !== id);
-    console.log(this.elemList);
-  }
-
-  handleStatus(id: string) {
-    const taskElement = document.getElementById('task')
-    const item = this.elemList.find((el) => el.id === id);
-    if (item) {
-      item.completed = !item.completed;
-    }
-
-    console.log(this.elemList)
-  }
   openDialog() {
     this.isDialogOpen = true;
     this.isDropShadowOpen = true;
---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { elemListType } from '../test-component/test-component';
+import { MatIconModule } from '@angular/material/icon';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.scss',
+  animations:[
+    trigger('addItem', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(100px)' }),
+        animate(
+          '200ms 200ms',
+          style({ opacity: 1, transform: 'translateY(0px)' })
+        ),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translateY(0px)' }),
+        animate('200ms', style({ opacity: 0, transform: 'translateY(100px)' })),
+      ]),
+    ]),
+  ]
+})
+export class TodoItemComponent {
+  @Input() elemList!: elemListType[];
+
+  handleStatus(id: string) {
+    const item = this.elemList.find((el) => el.id === id);
+    if (item) {
+      item.completed = !item.completed;
+    }
+  }
+  deleteElem(id: string) {
+    this.elemList = this.elemList.filter((el) => el.id !== id);
+  }
+}
