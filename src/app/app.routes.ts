e9960aee5778cf9afe42aaa35782b54b8e361ece
Eric Pezzulo <eric.pezzulo@gmail.com>
2024-08-16 10:56:28 -0400

    creating css game board, starting with mobile design first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { TodosComponent } from './todos-component/todos-component';
 import { JeopardyComponent } from './jeopardy/jeopardy.component';
 
-
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'user-page/:id', component: UserPageComponent },
   { path: 'todo', component: TodosComponent },
   // {path: 'todo/:id', component: TodoComponent}
-  {path: 'jeopardy', component: JeopardyComponent }
+  { path: 'jeopardy', component: JeopardyComponent },
 ];
